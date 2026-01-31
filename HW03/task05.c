8f81d94f5614cb953206beea68edf1159102608d
Alexey <alikanov@ipromo.ru>
2026-01-31 17:49:31 +0300

    Домашнее задание №3

---
+++ HW03/task05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c, d, e;
+	scanf("%d%d%d", &a, &b, &c);
+	d = a + b + c;
+	e = a * b * c;
+	printf("%d+%d+%d=%d\n", a, b, c, d);
+	printf("%d*%d*%d=%d", a, b, c, e);
+	return 0;
+}
+
---
+++ HW03/task08.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n, sum;
+	scanf("%d", &n);
+	sum = n%10;
+	sum *= (n/10)%10;
+	sum *= (n/100)%10;
+	printf("%d\n", sum);
+	return 0;
+}
+
