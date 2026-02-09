b99c766201675483dd912c1834e3933a6ae38709
Alexey <alikanov@ipromo.ru>
2026-02-09 13:34:26 +0300

    Домашнее задание №5

--- HW05/task_b21.c
+++ HW05/task_b21.c
@@ -5,7 +5,6 @@ int main(void)
 	char letter = ' ';
 	for (scanf("%c", &letter); letter != '.'; scanf("%c", &letter))
 	{
-		printf("%c\n", letter);
 		if (letter >= 'A' && letter <= 'Z')
 			letter += 0x20;
 		printf("%c", letter);
